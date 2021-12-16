b90b85b65fa2c4e62fa28c752722f2b0e181016f
jontaku <kalemtaku7608@gmail.com>
2021-12-16 15:19:46 +0900

    routinesコントローラーとフロント実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'routines/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
+  root to: "routines#index"
+  resources :routines, only:  [:index]
   resources :users, only: [:edit, :update]
 end
